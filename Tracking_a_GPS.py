2513f4e7c42042c0581662a9dedfe3209c8cde67
Mohammed Zia <mohammed.zia33@gmail.com>
2015-06-27 17:53:48 +0300

    Added Tracking_GPS_points.py

---
+++ Tracking_a_GPS.py
@@ -0,0 +1,22 @@
+import ast
+import requests
+
+response = requests.get('http://160.75.81.195:8080/postgis/pgr_aStarFromAtoB_without_SessionID/?long_st=28.4&lat_st=41.093&long_end=29.023&lat_end=41.0432')
+print (response.status_code)
+
+vectorLyr = QgsVectorLayer('Point?crs=epsg:4326', 'Layer 1' , "memory")
+vpr = vectorLyr.dataProvider()
+
+#The following code will convert string response into a json content
+geojson_dict = ast.literal_eval(response.content)
+
+for j in range(len(geojson_dict['features'])):
+	x = geojson_dict['features'][j]['geometry']['coordinates'][0][0]
+	y = geojson_dict['features'][j]['geometry']['coordinates'][0][1]
+	pnt = QgsGeometry.fromPoint(QgsPoint(x, y))
+	f = QgsFeature()
+	f.setGeometry(pnt)
+	vpr.addFeatures([f])
+
+vectorLyr.updateExtents()
+QgsMapLayerRegistry.instance().addMapLayer(vectorLyr)
