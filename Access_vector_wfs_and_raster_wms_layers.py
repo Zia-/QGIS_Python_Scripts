687d987db8c508ff2e56f75cd81cab4eef3740ca
Mohammed Zia <mohammed.zia33@gmail.com>
2015-07-02 14:39:10 +0300

    modified Access_vector_wfs_and_raster_wms_layers.py added wms vector and raster layers in one map canvas

--- Access_vector_wfs_and_raster_wms_layers.py
+++ Access_vector_wfs_and_raster_wms_layers.py
@@ -1,8 +1,21 @@
-params = {
-    'service': 'WFS',
-    'version': '1.0.0',
-    'request': 'GetFeature',
-    'typeName': 'topp:tasmania_state_boundaries',
-}
-uri = 'http://maps.itu.edu.tr:8082/geoserver/topp/ows?' + urllib.unquote(urllib.urlencode(params))
-vlayer = QgsVectorLayer( uri, "my_table", "ogr" )
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+import PyQt4.QtGui
+import urllib
+
+def run_script(iface):
+    params = {
+        'service': 'WFS',
+        'version': '1.0.0',
+        'request': 'GetFeature',
+        'typeName': 'topp:tasmania_state_boundaries',
+    }
+    uri = 'http://maps.itu.edu.tr:8082/geoserver/topp/ows?' + urllib.unquote(urllib.urlencode(params))
+    vlayer = QgsVectorLayer( uri, "my_table", "ogr" )
+    
+    urlWithParams = "url=http://maps.itu.edu.tr:8082/geoserver/sf/wms&format=image/png&layers=sfdem&styles=&crs=EPSG:26713"
+    rlayer = QgsRasterLayer(urlWithParams, 'DEM', 'wms')
+    
+    QgsMapLayerRegistry.instance().addMapLayers([rlayer, vlayer])
